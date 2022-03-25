c94f89a827bb2fa8953293aa63ad4be812b55d8a
Chamerli-sh <fares.setbels@gmail.com>
2022-03-25 13:07:57 +0100

    Separate things in two files

---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+use ggez::event::EventHandler;
+use ggez::{Context, GameResult};
+use ggez::graphics::{self, Color};
+
+pub struct MyGame {
+    // Your state here...
+}
+
+impl MyGame {
+    pub fn new(_ctx: &mut Context) -> MyGame {
+        // Load/create resources such as images here.
+        MyGame {
+            // ...
+        }
+    }
+}
+
+impl EventHandler for MyGame {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        // Update code here...
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx, Color::WHITE);
+        // Draw code here...
+        graphics::present(ctx)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+pub mod lib;
+
+use lib::MyGame;
+
 use ggez::{Context, ContextBuilder, GameResult};
-use ggez::graphics::{self, Color};
 use ggez::event::{self, EventHandler};
 
 fn main() {
@@ -15,30 +18,4 @@ fn main() {
 
     // Run!
     event::run(ctx, event_loop, my_game);
-}
-
-struct MyGame {
-    // Your state here...
-}
-
-impl MyGame {
-    pub fn new(_ctx: &mut Context) -> MyGame {
-        // Load/create resources such as images here.
-        MyGame {
-            // ...
-        }
-    }
-}
-
-impl EventHandler for MyGame {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
-        // Update code here...
-        Ok(())
-    }
-
-    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
-        graphics::clear(ctx, Color::WHITE);
-        // Draw code here...
-        graphics::present(ctx)
-    }
 }
